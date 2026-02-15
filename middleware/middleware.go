5bb5f42c8a399e58c3df9e87b866d3eff7eb2e2d
tuken <tuken@gmail.com>
2026-02-15 22:28:56 +0900

    update: middleware/middleware.go
    調整

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -2,50 +2,71 @@ package middleware
 
 import (
 	"context"
-	"log"
-	"net/url"
-	"strconv"
+	"time"
 
 	"github.com/99designs/gqlgen/graphql"
-	"gorm.io/driver/mysql"
+	"github.com/oklog/ulid/v2"
+	"github.com/tuken/nix/logger"
 	"gorm.io/gorm"
+	gormlog "gorm.io/gorm/logger"
 )
 
 type contextKey struct{ string }
 
 var (
-	DBKey = contextKey{"database"}
+	DBKey     = contextKey{"database"}
+	LoggerKey = contextKey{"logger"}
 )
 
-func DatabaseMiddleware(ctx context.Context, next graphql.OperationHandler) graphql.ResponseHandler {
-
-	log.Println("OperationMiddleware: called")
-
-	Host := "127.0.0.1"
-	Port := 3306
-	User := "root"
-	Password := "secualpass"
-	Database := "secual"
-	Timezone := "Asia/Tokyo"
-
-	dsn := User + ":" + url.QueryEscape(Password) + "@tcp(" + Host + ":" + strconv.Itoa(Port) + ")/" + Database + "?charset=utf8mb4&collation=utf8mb4_bin&parseTime=True&loc=" + url.QueryEscape(Timezone)
-
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatalf("failed to connect database: %v", err)
-	}
-
-	newCtx := context.WithValue(ctx, DBKey, db)
-
-	return next(newCtx)
+// QueryLogger handles query logging with DB and Logger access
+type QueryLogger struct {
+	DB          *gorm.DB
+	Log         logger.Logger
+	SQLLogLevel gormlog.LogLevel
 }
 
-func GetDatabase(ctx context.Context) *gorm.DB {
+// Middleware is the middleware function that logs queries and sets context values
+func (ql *QueryLogger) Middleware(ctx context.Context, next graphql.OperationHandler) graphql.ResponseHandler {
 
-	db, ok := ctx.Value(DBKey).(*gorm.DB)
-	if !ok {
-		log.Fatalf("failed to get database from context")
+	requestID := ulid.Make().String()
+
+	db := ql.DB.WithContext(ctx)
+
+	log := ql.Log.With("rid", requestID)
+	db.Logger = log.LogMode(ql.SQLLogLevel)
+
+	ctx = context.WithValue(ctx, DBKey, db)
+	ctx = context.WithValue(ctx, LoggerKey, log)
+
+	start := time.Now()
+
+	oc := graphql.GetOperationContext(ctx)
+
+	log.Infow("GraphQL Query Started", "operation", oc.OperationName, "query", oc.RawQuery, "variables", oc.Variables)
+
+	response := next(ctx)
+
+	duration := time.Since(start)
+
+	log.Infow("GraphQL Query Completed", "operation", oc.OperationName, "duration", duration)
+
+	return response
+}
+
+func MustDB(ctx context.Context) *gorm.DB {
+
+	if db, ok := ctx.Value(DBKey).(*gorm.DB); ok {
+		return db
 	}
 
-	return db
+	panic("missing db value in context")
+}
+
+func MustLogger(ctx context.Context) logger.Logger {
+
+	if log, ok := ctx.Value(LoggerKey).(logger.Logger); ok {
+		return log
+	}
+
+	panic("missing logger value in context")
 }
