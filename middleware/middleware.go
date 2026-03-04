381ba0b038a5c5601eb14792ac7e2bceadfabeab
tsukasa ikawa <tsukasa.ikawa@secual-inc.com>
2026-03-04 17:15:38 +0900

    update: middleware.go
    構成見直し（pipelineに処理移動）

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -5,79 +5,26 @@ import (
 	"time"
 
 	"github.com/99designs/gqlgen/graphql"
-	"github.com/oklog/ulid/v2"
-	"github.com/tuken/nix/db"
-	"github.com/tuken/nix/logger"
-	"gorm.io/gorm"
-	gormlog "gorm.io/gorm/logger"
+	"github.com/tuken/nix/pipeline"
 )
 
-type contextKey struct{ string }
+func LoggingMiddleware(ctx context.Context, next graphql.OperationHandler) graphql.ResponseHandler {
 
-var (
-	DBKey     = contextKey{"database"}
-	LoggerKey = contextKey{"logger"}
-	UserKey   = contextKey{"user"}
-)
-
-func MustDB(ctx context.Context) *gorm.DB {
-
-	if db, ok := ctx.Value(DBKey).(*gorm.DB); ok {
-		return db
-	}
-
-	panic("missing db value in context")
-}
-
-func MustLogger(ctx context.Context) logger.Logger {
-
-	if log, ok := ctx.Value(LoggerKey).(logger.Logger); ok {
-		return log
-	}
-
-	panic("missing logger value in context")
-}
-
-func MustUser(ctx context.Context) *db.User {
-
-	if user, ok := ctx.Value(UserKey).(*db.User); ok {
-		return user
-	}
-
-	panic("missing user value in context")
-}
-
-// QueryLogger handles query logging with DB and Logger access
-type QueryLogger struct {
-	DB          *gorm.DB
-	Log         logger.Logger
-	SQLLogLevel gormlog.LogLevel
-}
-
-// Middleware is the middleware function that logs queries and sets context values
-func (ql *QueryLogger) Middleware(ctx context.Context, next graphql.OperationHandler) graphql.ResponseHandler {
-
-	requestID := ulid.Make().String()
-
-	db := ql.DB.WithContext(ctx)
-
-	log := ql.Log.With("rid", requestID)
-	db.Logger = log.LogMode(ql.SQLLogLevel)
-
-	ctx = context.WithValue(ctx, DBKey, db)
-	ctx = context.WithValue(ctx, LoggerKey, log)
+	l := pipeline.MustLogger(ctx)
 
 	start := time.Now()
 
 	oc := graphql.GetOperationContext(ctx)
 
-	log.Infow("GraphQL Query Started", "operation", oc.OperationName, "query", oc.RawQuery, "variables", oc.Variables)
+	l.Infow("GraphQL Query Started", "operation", oc.OperationName, "query", oc.RawQuery, "variables", oc.Variables)
 
 	response := next(ctx)
 
 	duration := time.Since(start)
 
-	log.Infow("GraphQL Query Completed", "operation", oc.OperationName, "duration", duration)
+	defer func() {
+		l.Infow("GraphQL Query Completed", "operation", oc.OperationName, "duration", duration)
+	}()
 
 	return response
 }
