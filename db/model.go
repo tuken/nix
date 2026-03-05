88bd25cc005f068ee582ccd0b0f43736b0dc18de
tsukasa ikawa <tsukasa.ikawa@secual-inc.com>
2026-03-05 14:42:12 +0900

    update: db/model.go
    WeatherCode復活

--- db/model.go
+++ db/model.go
@@ -108,7 +108,7 @@ type WorkReport struct {
 	UpdatedBy     uint            `gorm:"not null;comment:'更新者ID'"`
 }
 
-// type WeatherCode struct {
-// 	Code     uint   `gorm:"primaryKey;comment:'天気コード'"`
-// 	Japanese string `gorm:"not null;comment:'天気の日本語表記'"`
-// }
+type WeatherCode struct {
+	Code     uint   `gorm:"primaryKey;comment:'天気コード'"`
+	Japanese string `gorm:"not null;comment:'天気の日本語表記'"`
+}
