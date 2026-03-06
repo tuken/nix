96bf6b60b3651d13fb2ce1cb76ae904123ca1b1d
tsukasa ikawa <tsukasa.ikawa@secual-inc.com>
2026-03-06 13:03:19 +0900

    update: works.resolvers.go
    CreateWorkReportのuserIDありなし版に対応
    works.goの共通関数使用

--- graph/works.resolvers.go
+++ graph/works.resolvers.go
@@ -10,8 +10,6 @@ import (
 	"fmt"
 
 	"github.com/jinzhu/copier"
-	"github.com/tuken/nix/aws"
-	"github.com/tuken/nix/conf"
 	"github.com/tuken/nix/db"
 	"github.com/tuken/nix/graph/model"
 	"github.com/tuken/nix/pipeline"
@@ -19,65 +17,14 @@ import (
 
 // CreateWorkReport is the resolver for the createWorkReport field.
 func (r *mutationResolver) CreateWorkReport(ctx context.Context, input model.CreateWorkReportInput) (*model.WorkReport, error) {
-	d := pipeline.MustDB(ctx)
-	l := pipeline.MustLogger(ctx)
 
-	isImage := input.Image != nil
+	return createWWorkReport(ctx, pipeline.MustUser(ctx).ID, input)
+}
 
-	newWorkReport := &db.WorkReport{
-		UserID:        uint(input.UserID),
-		FieldID:       uint(input.FieldID),
-		WorkDate:      input.WorkDate,
-		WorkTypeID:    uint(input.WorkTypeID),
-		CropVarietyID: uint(input.CropVarietyID),
-		WeatherCode:   uint(input.WeatherCode),
-		WorkDetail:    input.WorkDetail,
-		IsImage:       isImage,
-	}
+// CreateWorkReportWithUserID is the resolver for the createWorkReportWithUserID field.
+func (r *mutationResolver) CreateWorkReportWithUserID(ctx context.Context, userID int, input model.CreateWorkReportInput) (*model.WorkReport, error) {
 
-	if err := d.Create(newWorkReport).Error; err != nil {
-		l.Errorw("Error insert work_reports", "data", newWorkReport, "error", err)
-		return nil, err
-	}
-
-	s3 := aws.NewS3Client()
-
-	if isImage {
-
-		l.Infow("Upload", "filename", input.Image.Filename, "size", input.Image.Size, "mime", input.Image.ContentType)
-
-		key := fmt.Sprintf("WorkReports/%d.jpg", newWorkReport.ID)
-
-		if err := s3.Upload(input.Image.File, conf.S3BucketName, key, input.Image.ContentType); err != nil {
-
-			isImage = false
-			d.Where("id = ?", newWorkReport.ID).Update("is_image", false)
-
-			l.Errorw("Error upload s3 object", "key", key, "mime", input.Image.ContentType, "error", err)
-			return nil, err
-		}
-	}
-
-	dbWorkReport := db.WorkReport{}
-	if err := d.Preload("User").Preload("Field").Preload("WorkType").Preload("CropVariety").Preload("Weather").Find(&dbWorkReport, newWorkReport.ID).Error; err != nil {
-		return nil, err
-	}
-
-	workReport := model.WorkReport{}
-	copier.Copy(&workReport, &dbWorkReport)
-
-	if isImage {
-
-		url, err := s3.GetSignedURL(conf.S3BucketName, fmt.Sprintf("WorkReports/%d.jpg", newWorkReport.ID))
-		if err != nil {
-			l.Errorw("Error get signed URL", "key", fmt.Sprintf("WorkReports/%d.jpg", newWorkReport.ID), "error", err)
-			return nil, err
-		}
-
-		workReport.ImageURL = &url
-	}
-
-	return &workReport, nil
+	return createWWorkReport(ctx, uint(userID), input)
 }
 
 // WorkTypes is the resolver for the workTypes field.
