f8cb27f6d7c5ec03bdc055feffeb4e599f63624e
tsukasa ikawa <tsukasa.ikawa@secual-inc.com>
2026-03-06 13:50:10 +0900

    update: works.go
    Userフィールドコピーは不要だったので削除

--- graph/works.go
+++ graph/works.go
@@ -60,7 +60,6 @@ func createWWorkReport(ctx context.Context, userID uint, input model.CreateWorkR
 
 	workReport := model.WorkReport{}
 	copier.Copy(&workReport, &dbWorkReport)
-	copier.Copy(&workReport.User, &dbWorkReport.User)
 
 	if isImage {
 
