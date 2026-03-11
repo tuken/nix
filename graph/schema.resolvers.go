a94db116a211f2c032748c9047ea4f3157b16c20
tsukasa ikawa <tsukasa.ikawa@secual-inc.com>
2026-03-11 11:42:40 +0900

    update: schema.resolvers.go
    WorkTypeSummary内、SQLのwork_typesをwtに
    sams → sums

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -204,7 +204,7 @@ func (r *queryResolver) WorkTypeSummary(ctx context.Context) ([]*model.WorkTypeS
 	d := pipeline.MustDB(ctx)
 	l := pipeline.MustLogger(ctx)
 
-	sams := []struct {
+	sums := []struct {
 		WorkTypeID uint
 		db.WorkType
 		Today     int
@@ -214,14 +214,14 @@ func (r *queryResolver) WorkTypeSummary(ctx context.Context) ([]*model.WorkTypeS
 		ThisMonth int
 	}{}
 
-	if err := d.Model(&db.WorkReport{}).Select("work_reports.work_type_id, work_types.*, " + "SUM(CASE WHEN work_date >= CURDATE() AND work_date < CURDATE() + INTERVAL 1 DAY THEN 1 ELSE 0 END) AS today, " + "SUM(CASE WHEN work_date >= CURDATE() - INTERVAL 1 DAY AND work_date < CURDATE() THEN 1 ELSE 0 END) AS yesterday, " + "SUM(CASE WHEN YEARWEEK(work_date, 0) = YEARWEEK(CURDATE(), 0) THEN 1 ELSE 0 END) AS this_week, " + "SUM(CASE WHEN YEARWEEK(work_date, 0) = YEARWEEK(CURDATE() - INTERVAL 7 DAY, 0) THEN 1 ELSE 0 END) AS last_week, " + "SUM(CASE WHEN YEAR(work_date) = YEAR(CURDATE()) AND MONTH(work_date) = MONTH(CURDATE()) THEN 1 ELSE 0 END) AS this_month").Joins("INNER JOIN work_types wt ON wt.id = work_reports.work_type_id").Group("work_reports.work_type_id").Order("work_reports.work_type_id").Find(&sams).Error; err != nil {
+	if err := d.Model(&db.WorkReport{}).Select("work_reports.work_type_id, wt.*, " + "SUM(CASE WHEN work_date >= CURDATE() AND work_date < CURDATE() + INTERVAL 1 DAY THEN 1 ELSE 0 END) AS today, " + "SUM(CASE WHEN work_date >= CURDATE() - INTERVAL 1 DAY AND work_date < CURDATE() THEN 1 ELSE 0 END) AS yesterday, " + "SUM(CASE WHEN YEARWEEK(work_date, 0) = YEARWEEK(CURDATE(), 0) THEN 1 ELSE 0 END) AS this_week, " + "SUM(CASE WHEN YEARWEEK(work_date, 0) = YEARWEEK(CURDATE() - INTERVAL 7 DAY, 0) THEN 1 ELSE 0 END) AS last_week, " + "SUM(CASE WHEN YEAR(work_date) = YEAR(CURDATE()) AND MONTH(work_date) = MONTH(CURDATE()) THEN 1 ELSE 0 END) AS this_month").Joins("INNER JOIN work_types wt ON wt.id = work_reports.work_type_id").Group("work_reports.work_type_id").Order("work_reports.work_type_id").Find(&sums).Error; err != nil {
 		l.Errorw("Error work_reports", "error", err)
 		return nil, fmt.Errorf("error work_reports: %v", err)
 	}
 
-	items := make([]*model.WorkTypeSummaryItem, len(sams))
+	items := make([]*model.WorkTypeSummaryItem, len(sums))
 
-	for _, sam := range sams {
+	for _, sam := range sums {
 
 		i := model.WorkTypeSummaryItem{}
 		copier.Copy(&i.WorkType, &sam.WorkType)
