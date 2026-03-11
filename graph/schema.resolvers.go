b788b1914db133cc4f9fc7190ab315b0d8c3abef
tsukasa ikawa <tsukasa.ikawa@secual-inc.com>
2026-03-11 11:54:35 +0900

    update: schema.resolvers.go
    makeでのスライス作成をやめる

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -218,7 +218,7 @@ func (r *queryResolver) WorkTypeSummary(ctx context.Context) ([]*model.WorkTypeS
 		return nil, fmt.Errorf("error work_reports: %v", err)
 	}
 
-	items := make([]*model.WorkTypeSummaryItem, len(sums))
+	items := []*model.WorkTypeSummaryItem{}
 
 	for _, sam := range sums {
 
